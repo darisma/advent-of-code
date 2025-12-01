bbd1b73a8f7350b6f8a40f47e0b3441cdb7463bf
Tatu Suopajärvi <tatu.suopajarvi@tietoevry.com>
2025-12-01 11:19:05 +0200

    day1

--- src/advent2025/Day1.java
+++ src/advent2025/Day1.java
@@ -6,87 +6,71 @@ import java.util.stream.Collectors;
 import utils.AdventInputReader;
 
 public class Day1 {
-
-    static AdventInputReader ir = new AdventInputReader();
     static final String FILENAME = "2025input/day1.txt";
-
-    static final List<String> rotationData = ir.getStringStream(FILENAME).collect(Collectors.toList());
+    static final List<String> rotationData = new AdventInputReader().getStringStream(FILENAME).collect(Collectors.toList());
+    static final List<Rotation> rotations = rotationData.stream().map(Day1::parseRotation).collect(Collectors.toList());
 
     public static void main(String[] args) {
-        System.out.println("Answer to 1a is: " + solveAdvent1a());
-        System.out.println("Answer to 1b is: " + solveAdvent1b());
+        System.out.println("Answer to 1a is: " + solveAdvent1a(rotationData));
+        System.out.println("Answer to 1b is: " + solveAdvent1b(rotationData));
     }
 
-    private static int solveAdvent1a() {
+    private static class Rotation {
+        final char direction;
+        final int amount;
+        Rotation(char direction, int amount) {
+            this.direction = direction;
+            this.amount = amount;
+        }
+    }
+
+    private static Rotation parseRotation(String rotation) {
+        return new Rotation(rotation.charAt(0), Integer.parseInt(rotation.substring(1)));
+    }
+
+    private static int solveAdvent1a(List<String> data) {
         int pointer = 50;
         int timesResultedIn0 = 0;
-
-        for(String rotation : rotationData) {
-            String direction = rotation.substring(0, 1);
-            int amount = Integer.parseInt(rotation.substring(1));
-
-            if (direction.equals("L")) {
-                pointer = Math.floorMod(pointer - amount, 100);
-                if (pointer == 0) {
-                    timesResultedIn0++;
-                }
-            } else if (direction.equals("R")) {
-                pointer = (pointer + amount) % 100;
-                if (pointer == 0) {
-                    timesResultedIn0++;
-                }
+        for (Rotation rotation: rotations) {
+            // tehdään pyöräytys ja katsotaan osuiko nollaan
+            if (rotation.direction == 'L') {
+                pointer = Math.floorMod(pointer - rotation.amount, 100);
+            } else if (rotation.direction == 'R') {
+                pointer = (pointer + rotation.amount) % 100;
+            }
+            if (pointer == 0) {
+                timesResultedIn0++;
             }
         }
-
         return timesResultedIn0;
     }
 
-    private static long solveAdvent1b() {
+    private static long solveAdvent1b(List<String> data) {
         int pointer = 50;
         int timesAt0 = 0;
-
-        for(String rotation : rotationData) {
-            String direction = rotation.substring(0, 1);
-            int amount = Integer.parseInt(rotation.substring(1));
-
-            if (direction.equals("L")) {
-
-                if(pointer == 0) {
-                    pointer = pointer - amount;
-
+        for (String rotationStr : data) {
+            boolean wasAt0 = (pointer == 0); // pyörähdys lähtee nollasta ?
+            Rotation rotation = parseRotation(rotationStr);
+            if (rotation.direction == 'L') {
+                    pointer -= rotation.amount;
                     while (pointer < 0) {
                         timesAt0++;
-                        pointer = pointer + 100;
+                        pointer += 100;
                     }
-                    if(pointer == 0) {
+                    if (pointer == 0) {
                         timesAt0++;
                     }
-                    timesAt0--; // lähdettiin 0:sta, ei lasketa sitä
-                }else {
-                    pointer = pointer - amount;
-
-                    while (pointer < 0) {
-                        timesAt0++;
-                        pointer = pointer + 100;
+                    if (wasAt0) {
+                        timesAt0--; // started from 0, don't count it
                     }
-                    if(pointer == 0) {
-                        timesAt0++;
-                    }
-                }
-
-
-            } else if (direction.equals("R")) {
-
-                pointer = pointer + amount;
-
-                while(pointer >= 100) {
+            } else if (rotation.direction == 'R') {
+                pointer += rotation.amount;
+                while (pointer >= 100) {
                     timesAt0++;
-                    pointer = pointer - 100;
+                    pointer -= 100;
                 }
             }
-
         }
-
         return timesAt0;
     }
 }
